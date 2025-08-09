25bcfb0f1cb7d747d91138793ba349e8975445bf
Valerie Lavoisier <val.lavoisier3@gmail.com>
2025-08-09 20:39:06 +0200

    recupération des détails d'une oeuvre depuis la BDD dans oeuvre.php et suppression du fichier oeuvres.php

--- oeuvre.php
+++ oeuvre.php
@@ -1,26 +1,22 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+    require 'bdd.php';
+
+    $bdd = connexion();
+    $requete = $bdd->prepare("SELECT * FROM oeuvres WHERE id = ?");
+    $requete->execute([$_GET['id']]);
+    $oeuvre = $requete->fetch();
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
-    }
-
-    $oeuvre = null;
-
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
+        exit;
     }
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
     if(is_null($oeuvre)) {
         header('Location: index.php');
+        exit;
     }
 ?>
 
@@ -38,3 +34,4 @@
 </article>
 
 <?php require 'footer.php'; ?>
+
